c112dbcf77ff95f10de1836388131e51f7d3cf05
Allison Vacanti <alliepiper16@gmail.com>
2021-03-16 18:30:55 -0400

    Expand `cub::DeviceRadixSort` coverage.
    Bumps NVBench for new functionality.

--- benches/cub/device/radix_sort/sort_keys_descending.cu
+++ benches/cub/device/radix_sort/sort_keys_descending.cu
@@ -1,12 +1,10 @@
-#include <nvbench/nvbench.cuh>
-
 #include <cub/device/device_radix_sort.cuh>
 
-#include <thrust/detail/raw_pointer_cast.h>
+#include <nvbench/nvbench.cuh>
+
+#include "radix_sort_input_generator.cuh"
+
 #include <thrust/device_vector.h>
-#include <thrust/random.h>
-#include <thrust/sequence.h>
-#include <thrust/shuffle.h>
 
 template <typename KeyType>
 void sort_keys_descending(nvbench::state &state, nvbench::type_list<KeyType>)
@@ -14,51 +12,59 @@ void sort_keys_descending(nvbench::state &state, nvbench::type_list<KeyType>)
   // Retrieve axis parameters
   const auto elements = static_cast<std::size_t>(state.get_int64("Elements"));
 
-  // Enable throughput calculations and add "Size" column to results.
+  // Enable throughput calculations and add "Size" column
+  // to results.
   state.add_element_count(elements);
   state.add_global_memory_reads<KeyType>(elements, "Size");
   state.add_global_memory_writes<KeyType>(elements);
 
   // Allocate and initialize data:
-  thrust::device_vector<KeyType> input(elements);
+  auto input_gen                       = input_generator{elements, "Random"};
+  thrust::device_vector<KeyType> input = input_gen.generate<KeyType>();
   thrust::device_vector<KeyType> output(elements, {});
-  thrust::sequence(input.begin(), input.end());
-  thrust::shuffle(input.begin(), input.end(), thrust::default_random_engine{});
+
+  // Prepare kernel args:
+  const auto *keys_in  = thrust::raw_pointer_cast(input.data());
+  auto *keys_out       = thrust::raw_pointer_cast(output.data());
+  const auto begin_bit = int{0};
+  const auto end_bit   = static_cast<int>(sizeof(KeyType) * 8);
 
   // Allocate temporary storage:
   std::size_t temp_size;
   cub::DeviceRadixSort::SortKeysDescending(nullptr,
                                            temp_size,
-                                           static_cast<const KeyType *>(
-                                             nullptr),
-                                           static_cast<KeyType *>(nullptr),
+                                           keys_in,
+                                           keys_out,
                                            static_cast<int>(elements),
-                                           0,
-                                           sizeof(KeyType) * 8);
+                                           begin_bit,
+                                           end_bit);
   thrust::device_vector<nvbench::uint8_t> temp(temp_size);
+  auto *temp_storage = thrust::raw_pointer_cast(temp.data());
 
-  state.exec([keys_in      = thrust::raw_pointer_cast(input.data()),
-              keys_out     = thrust::raw_pointer_cast(output.data()),
-              temp_storage = thrust::raw_pointer_cast(temp.data()),
-              temp_size,
-              elements](nvbench::launch &launch) {
-    auto temp_size_lvalue = temp_size; // for CUB API
-    cub::DeviceRadixSort::SortKeysDescending(temp_storage,
-                                             temp_size_lvalue,
-                                             keys_in,
-                                             keys_out,
-                                             static_cast<int>(elements),
-                                             0,
-                                             sizeof(KeyType) * 8,
-                                             launch.get_stream());
-  });
+  state.exec(nvbench::exec_tag::timer,
+             [&](nvbench::launch &launch, auto &timer) {
+               // Do reset:
+               input_gen.reset(input);
+               keys_in = thrust::raw_pointer_cast(input.data());
+
+               timer.start();
+               cub::DeviceRadixSort::SortKeysDescending(temp_storage,
+                                                        temp_size,
+                                                        keys_in,
+                                                        keys_out,
+                                                        static_cast<int>(
+                                                          elements),
+                                                        begin_bit,
+                                                        end_bit,
+                                                        launch.get_stream());
+               timer.stop();
+             });
 }
-using types = nvbench::type_list<nvbench::uint8_t,
-                                 nvbench::uint16_t,
-                                 nvbench::uint32_t,
-                                 nvbench::uint64_t,
-                                 nvbench::float32_t,
-                                 nvbench::float64_t>;
-NVBENCH_BENCH_TYPES(sort_keys_descending, NVBENCH_TYPE_AXES(types))
+
+// Only spot-check a few common key types. sort_keys provides more exhaustive
+// coverage.
+using common_key_types =
+nvbench::type_list<nvbench::uint8_t, nvbench::int32_t, nvbench::float64_t>;
+NVBENCH_BENCH_TYPES(sort_keys_descending, NVBENCH_TYPE_AXES(common_key_types))
   .set_name("cub::DeviceRadixSort::SortKeysDescending")
   .add_int64_power_of_two_axis("Elements", nvbench::range(20, 30, 2));
--- benches/cub/device/radix_sort/sort_pairs_descending.cu
+++ benches/cub/device/radix_sort/sort_pairs_descending.cu
@@ -1,12 +1,10 @@
-#include <nvbench/nvbench.cuh>
-
 #include <cub/device/device_radix_sort.cuh>
 
-#include <thrust/detail/raw_pointer_cast.h>
+#include <nvbench/nvbench.cuh>
+
+#include "radix_sort_input_generator.cuh"
+
 #include <thrust/device_vector.h>
-#include <thrust/random.h>
-#include <thrust/sequence.h>
-#include <thrust/shuffle.h>
 
 template <typename KeyType, typename ValueType>
 void sort_pairs_descending(nvbench::state &state,
@@ -23,57 +21,65 @@ void sort_pairs_descending(nvbench::state &state,
   state.add_global_memory_writes<ValueType>(elements);
 
   // Allocate and initialize data:
-  thrust::device_vector<KeyType> input_keys(elements);
+  auto input_gen = input_generator{elements, "Random"};
+  thrust::device_vector<KeyType> input_keys = input_gen.generate<KeyType>();
   thrust::device_vector<KeyType> output_keys(elements, {});
   thrust::device_vector<ValueType> input_values(elements);
   thrust::device_vector<ValueType> output_values(elements, {});
-  thrust::sequence(input_keys.begin(), input_keys.end());
   thrust::sequence(input_values.begin(), input_values.end());
-  thrust::shuffle(input_keys.begin(),
-                  input_keys.end(),
-                  thrust::default_random_engine{});
+
+  // Prepare kernel args:
+  const auto *keys_in   = thrust::raw_pointer_cast(input_keys.data());
+  auto *keys_out        = thrust::raw_pointer_cast(output_keys.data());
+  const auto *values_in = thrust::raw_pointer_cast(input_values.data());
+  auto *values_out      = thrust::raw_pointer_cast(output_values.data());
+  const auto begin_bit  = int{0};
+  const auto end_bit    = static_cast<int>(sizeof(KeyType) * 8);
 
   // Allocate temporary storage:
   std::size_t temp_size;
-  cub::DeviceRadixSort::SortPairsDescending(
-    nullptr,
-    temp_size,
-    static_cast<const KeyType *>(nullptr),
-    static_cast<KeyType *>(nullptr),
-    static_cast<const ValueType *>(nullptr),
-    static_cast<ValueType *>(nullptr),
-    static_cast<int>(elements),
-    0,
-    sizeof(KeyType) * 8);
+  cub::DeviceRadixSort::SortPairsDescending(nullptr,
+                                            temp_size,
+                                            keys_in,
+                                            keys_out,
+                                            values_in,
+                                            values_out,
+                                            static_cast<int>(elements),
+                                            begin_bit,
+                                            end_bit);
   thrust::device_vector<nvbench::uint8_t> temp(temp_size);
+  auto *temp_storage = thrust::raw_pointer_cast(temp.data());
 
-  state.exec([keys_in      = thrust::raw_pointer_cast(input_keys.data()),
-              keys_out     = thrust::raw_pointer_cast(output_keys.data()),
-              values_in    = thrust::raw_pointer_cast(input_values.data()),
-              values_out   = thrust::raw_pointer_cast(output_values.data()),
-              temp_storage = thrust::raw_pointer_cast(temp.data()),
-              temp_size,
-              elements](nvbench::launch &launch) {
-    auto temp_size_lvalue = temp_size; // for CUB API
-    cub::DeviceRadixSort::SortPairsDescending(temp_storage,
-                                              temp_size_lvalue,
-                                              keys_in,
-                                              keys_out,
-                                              values_in,
-                                              values_out,
-                                              static_cast<int>(elements),
-                                              0,
-                                              sizeof(KeyType) * 8,
-                                              launch.get_stream());
-  });
+  state.exec(nvbench::exec_tag::timer,
+             [&](nvbench::launch &launch, auto &timer) {
+               // Do reset:
+               input_gen.reset(input_keys);
+               keys_in = thrust::raw_pointer_cast(input_keys.data());
+
+               timer.start();
+               cub::DeviceRadixSort::SortPairsDescending(temp_storage,
+                                                         temp_size,
+                                                         keys_in,
+                                                         keys_out,
+                                                         values_in,
+                                                         values_out,
+                                                         static_cast<int>(
+                                                           elements),
+                                                         begin_bit,
+                                                         end_bit,
+                                                         launch.get_stream());
+               timer.stop();
+             });
 }
-using types = nvbench::type_list<nvbench::uint8_t,
-                                 nvbench::uint16_t,
-                                 nvbench::uint32_t,
-                                 nvbench::uint64_t,
-                                 nvbench::float32_t,
-                                 nvbench::float64_t>;
-NVBENCH_BENCH_TYPES(sort_pairs_descending, NVBENCH_TYPE_AXES(types, types))
-  .set_name("cub::DeviceRadixSort::SortPairs")
+// Only spot-check a few common key types. sort_keys provides more exhaustive
+// coverage.
+using common_key_types =
+  nvbench::type_list<nvbench::uint8_t, nvbench::int32_t, nvbench::float64_t>;
+// Value types don't really matter much, they just get permuted.
+// Only benchmark with a 32-bit payload.
+using value_types = nvbench::type_list<nvbench::uint32_t>;
+NVBENCH_BENCH_TYPES(sort_pairs_descending,
+                    NVBENCH_TYPE_AXES(common_key_types, value_types))
+  .set_name("cub::DeviceRadixSort::SortPairsDescending")
   .set_type_axes_names({"Key", "Value"})
   .add_int64_power_of_two_axis("Elements", nvbench::range(20, 30, 2));
